a0c6fb34beb65e47abc5a345e707eecd8fd28158
Paola Merelli <paola.merelli@studio.unibo.it>
2026-04-30 17:04:58 +0000

    refinement to obtain potential biomass spatially (for 1 year, 2017): AGB_2017_100m resampled to AD_1ha resolution keeping q3 (custom method in terra::resample) for IAVS analysis i'll stop here, i've obtained poitential AGB for 2017 however i still need to: - implement loop for processing all years; - script for temporal quantile - actually understand which procedure is better: spatial first then temporal seems the most feasible in terms of RAM and Time, but not conceptually, IMO.

--- script/Create_AGB_potential.R
+++ script/Create_AGB_potential.R
@@ -2,43 +2,61 @@ library(sf)
 library(terra)
 library(parallel)
 
-###### devo provare a scaricare un altro anno, metterlo nella stessa cartella e fare un loop in modo che mi faccia automaticamente gli anni 
-######## provare a parallelizzare i tiles
+# sono ancora indecisa se per la biomassa potenziale sia meglio fare prima temporale e poi spaziale o viceversa
+# mi sembra piu sensato fare prima il temporale, ma sembra infattibile in termini di memoria (10 raster globali, uno per anno a 100m res)
 
-# Sabatini Alpha Diversity
-AD_1ha <- rast("03.Data/in/Sabatini_AlphaDiversity/w3_tile_sr1ha_for.tif")
-res(AD_1ha)
-plot(AD_1ha)
+ 
 
-# esa agb
+######## ora faccio lo spaziale su un anno solo. 
+###### devo provare a scaricare un altro anno, metterlo nella stessa cartella e fare un loop in modo che mi faccia automaticamente gli anni
+
+#####resample con method q3 su vrt globale 
+
+
 C_list <- list.files("03.Data/in/dap.ceda.ac.uk/", pattern ="*.tif$", full.names = TRUE, recursive = TRUE)
-C_rast <- C_list[str_detect(C_list, "SD", negate = TRUE)] |>
-  lapply(FUN = terra::rast)
-
-C_res <- lapply(seq_along(C_rast), function(i) {
-  out <- paste0("03.Data/out/agb_resampled/res_", i, ".tif")
-  C_rast[[i]] |>
-    terra::resample(AD_1ha, method "q3") |>
-    terra::writeraster(out, overwrite = TRUE)
-  gc()
-  out
-})
+C_path <- C_list[str_detect(C_list, "SD", negate = TRUE)]
+AD_1ha <- terra::rast("03.Data/in/Sabatini_AlphaDiversity/w3_tile_sr1ha_for.tif")
 
 vrt_file <- "agb_mosaic.vrt"
-terra::vrt(unlist(C_res), filename = vrt_file, overwrite = T)
+terra::vrt(C_path, filename = vrt_file, overwrite = TRUE)
 C_map <- terra::rast(vrt_file)
 plot(C_map)
 
-terra::writeRaster(
-  C_map,
-  "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017.tif",
-  overwrite = TRUE,
-  gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BLOCKXSIZE=512", "BLOCKYSIZE=512")
+#C_test <- terra::crop(C_map, terra::ext(-10, 10, -10, 10))
+#AD_test <- terra::crop(AD_1ha, terra::ext(-10, 10, -10, 10))
+
+#system.time(terra::resample(C_test, AD_test, method = "q3"))
+# 12 sec per 20°x20°
+
+system.time(
+  terra::resample(
+    C_map,
+    AD_1ha,
+    method = "q3",
+    filename = "03.Data/out/AGB_mosaic_2017.tif",
+    overwrite = TRUE,
+    gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIFF=YES")
+  )
 )
 
-unlink(unlist(C_res))
-unlink(vrt_file)
-gc()
+# 15 min e ca 28G RAM
+
+agb_mosaic_2017 <- terra::rast("03.Data/out/AGB_mosaic_2017.tif")
+plot(agb_mosaic_2017)
+
+############### devo ancora capire se è meglio aggregate o resample.
+# x ora ho usato resample perchè ha come custom il method q3. claude dice che aggregate con
+# funzione custom per il quantile è piu rischioso comunque fx sarebbe: 
+
+terra::aggregate(
+  C_map,
+  fact = round(0.0416667 / res(C_map)[1]),
+  fun = function(x, na.rm = TRUE) quantile(x, 0.75, na.rm = na.rm),
+  na.rm = TRUE,
+  filename = "03.Data/out/AGB_mosaic_2017.tif",
+  overwrite = TRUE,
+  gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIFF=YES")
+)
 
 ############# temporal quantile
 
