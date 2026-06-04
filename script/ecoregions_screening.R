5bdcddfd13577ac9cd810cc028fc9f762aa8f2a6
Paola Merelli <paola.merelli@studio.unibo.it>
2026-06-04 14:40:20 +0000

    added n_pixel for each ecoregion in the df + eco_filt stronger which filter ecoregions with min 100 pixels

--- script/ecoregions_screening.R
+++ script/ecoregions_screening.R
@@ -17,11 +17,16 @@ A_eco <- zonal(A_pix_ha, eco_rast, fun = "sum", na.rm = TRUE)
 A_for_ha <- A_pix_ha * forest_mask
 #calculate area of forested pixels for each ecoregion
 A_eco_for <- zonal(A_for_ha, eco_rast, fun = "sum", na.rm = TRUE)
+#calculate number of forested pixels for each ecoregion
+n_pix_for <- zonal(forest_mask, eco_rast, fun = "sum", na.rm = TRUE)
 
 eco_for_df <- A_eco |>
   rename(ECO_NAME = 1, total_ha = 2) |>
   left_join(A_eco_for |>
               rename(ECO_NAME = 1, forest_ha = 2),
+            by = "ECO_NAME") |>
+  left_join(n_pix_for |>
+              rename(ECO_NAME = 1, n_pix_for = 2),
             by = "ECO_NAME") |>
   mutate(forest_perc = (forest_ha/total_ha) * 100) |>
   arrange(desc(forest_perc))
@@ -29,6 +34,9 @@ eco_for_df <- A_eco |>
 
 eco_filt <- eco_for_df |>
   filter(forest_ha >= 100000 | forest_perc >= 10)
-#479
+
+eco_filt_stronger <- eco_for_df |>
+  filter((forest_ha >= 100000 | forest_perc >= 10) & n_pix_for >= 100)
 
 saveRDS(eco_filt, "03.Data/out/df_ecor_filtered.RDS")
+saveRDS(eco_filt_stronger, "03.Data/out/df_ecor_filtered_stronger.RDS")
