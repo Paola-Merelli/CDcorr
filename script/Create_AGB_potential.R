93e0d87a4bd5ba5502af5efc5ed0a56c1554b0e0
Paola Merelli <paola.merelli@studio.unibo.it>
2026-09-04 17:37:14 +0000

    spatial resampling of 2017 AGB layer but taking as the final value for each 2.5 arcmin resampled cell the q90 of AGB != 0 100m pixels

--- script/Create_AGB_potential.R
+++ script/Create_AGB_potential.R
@@ -3,87 +3,81 @@ library(terra)
 library(stringr)
 
 
-######## ora faccio lo spaziale su un anno solo. 
-###### devo provare a scaricare un altro anno, metterlo nella stessa cartella e fare un loop in modo che mi faccia automaticamente gli anni
+######## for now just spatial resampling on a single AGB layer (2017) 
+##### resample con q90 su vrt globale, ma q90 solo dei forested pixels dentro 2.5 arcminute cell (forested pixels = AGB > 0)
 
-#####resample con q0.95 su vrt globale 
-
-
-C_list <- list.files("03.Data/in/dap.ceda.ac.uk/", pattern ="*.tif$", full.names = TRUE, recursive = TRUE)
+C_list <- list.files("03.Data/in/dap.ceda.ac.uk.v6/", pattern ="*.tif$", full.names = TRUE, recursive = TRUE)
 C_path <- C_list[str_detect(C_list, "SD", negate = TRUE)]
-AD_map <- terra::rast("03.Data/in/Sabatini_AlphaDiversity/w3_tile_sr1000_for.tif")
+AD_map <- terra::rast("03.Data/in/Sabatini_AD/w3_tile_sr1000_for.tif")
 
 vrt_file <- "agb_mosaic.vrt"
 terra::vrt(C_path, filename = vrt_file, overwrite = TRUE)
 C_map <- terra::rast(vrt_file)
 plot(C_map)
 
-#C_test <- terra::crop(C_map, terra::ext(-10, 10, -10, 10))
-#AD_test <- terra::crop(AD_map, terra::ext(-10, 10, -10, 10))
 
-#system.time(terra::resample(C_test, AD_test, method = "q3"))
-# 12 sec per 20°x20°
-
-system.time(
-  terra::resample(
-    C_map,
-    AD_map,
-    method = "q3",
-    filename = "03.Data/out/AGB_mosaic_2017.tif",
-    overwrite = TRUE,
-    gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIFF=YES")
-  )
-)
-
-# 15 min e ca 28G RAM
-
-agb_mosaic_2017 <- terra::rast("03.Data/out/AGB_mosaic_2017.tif")
-plot(agb_mosaic_2017)
-
-# x ora ho usato resample perchè ha come custom il method q3. claude dice che aggregate con
-# per fare 0.95 mi serve una funzione custom quindi devo usare aggregate 
 C_test <- terra::crop(C_map, terra::ext(-10, 10, -10, 10))
 
+terraOptions(memfrac = 0.3)
+system.time(terra::aggregate(
+  C_test,
+  fact = round(0.0416667 / res(C_test)[1]),
+  fun = function(x, na.rm = TRUE) {
+    x_notz <- x[x != 0 & !is.na(x)]
+    frac <- length(x_notz) / length(x[!is.na(x)])
+    if (frac < 0.10 || length(x_notz) == 0) return(NA)
+    quantile(x_notz, 0.90)
+  },
+  na.rm = TRUE,
+  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_test.tif",
+  overwrite = TRUE
+))
 
-system.time(terra::aggregate(C_test, 
-                             fact = round(0.041667 / res(C_test)[1]),
-                             fun = function(x, na.rm = TRUE) quantile(x, 0.95, na.rm = na.rm),
-                             na.rm = TRUE))
-                             
-# 23 sec per 20°x20°
+#user  system elapsed 
+#19.187   0.217  19.335 
+file.remove("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_test.tif")
+
 
 terraOptions(memfrac = 0.3)
-# pupoi provare a aggiungere argomento ncores in aggregate. così ci mette 65 min e ca 110 GB
 
 system.time(terra::aggregate(
   C_map,
   fact = round(0.0416667 / res(C_map)[1]),
-  fun = function(x, na.rm = TRUE) quantile(x, 0.95, na.rm = na.rm),
+  fun = function(x, na.rm = TRUE) {
+    x_notz <- x[x != 0 & !is.na(x)]
+    frac <- length(x_notz) / length(x[!is.na(x)])
+    if (frac < 0.10 || length(x_notz) == 0) return(NA)
+    quantile(x_notz, 0.90)
+    },
   na.rm = TRUE,
-  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95_tmp.tif",
+  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_tmp.tif",
   overwrite = TRUE
 ))
 
+#user   system  elapsed 
+#2347.805  965.275 3307.308 
+#55 min sui 50 GB #valuta se aumentare memfrac
 
 terra::writeRaster(
-  terra::rast("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95_tmp.tif"),
-  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95.tif",
-  gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIFF=YES"),
+  terra::rast("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_tmp.tif"),
+  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for.tif",
+  gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIF=YES"),
   overwrite = TRUE
 )
 
-# Rimuovi il tmp
-file.remove("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95_tmp.tif")
+file.remove("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_tmp.tif")
 rm(vrt_file)
 gc()
 
-agb_agg <- terra::rast("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95.tif")
+agb_agg <- terra::rast("03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for.tif")
+plot(agb_agg)
+res(agb_agg)
 
 terra::resample(
   agb_agg,
   AD_map,
   method = "near",   
-  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q95.tif",
+  filename = "03.Data/out/agb_mosaic_yy/AGB_mosaic_2017_q90_for_final.tif",
   overwrite = TRUE,
   gdal = c("COMPRESS=DEFLATE", "TILED=YES", "BIGTIFF=YES")
 )
@@ -92,4 +86,4 @@ terra::resample(
 
 
 
-############## sensitivity analysis doing it with different % and with the mean
+############## sensitivity analysis doing it with different % and with the mean & median
